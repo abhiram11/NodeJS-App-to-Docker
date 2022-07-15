30c08203765aebadd916114723427275b33653d4
abhiram11 <abhiramsatpute@gmail.com>
2022-07-15 15:07:16 +0530

    initialized with nodejs, index and express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("You hit the homepage!");
+});
+
+app.listen(port, () => {
+  console.log("Connected open on Port:", port);
+});
